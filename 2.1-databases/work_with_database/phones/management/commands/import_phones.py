ffe1ba8f7c4e5a94eb54d6e71997dc45c844a41f
Sergey Romanov <serhromanov23@gmail.com>
2025-06-30 21:37:37 +0300

    2.1-work_with_database commit

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -1,7 +1,8 @@
 import csv
-
+from datetime import datetime
 from django.core.management.base import BaseCommand
 from phones.models import Phone
+from django.utils.text import slugify
 
 
 class Command(BaseCommand):
@@ -13,5 +14,14 @@ class Command(BaseCommand):
             phones = list(csv.DictReader(file, delimiter=';'))
 
         for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+            lte_exists = phone['lte_exists'].strip().lower() == 'true'
+            
+            Phone.objects.create(
+                name = phone['name'],
+                price = float(phone['price']),
+                image = phone['image'],
+                release_date = datetime.strptime(phone['release_date'], '%Y-%m-%d').date(),
+                lte_exists = lte_exists,
+                slug = slugify(phone['name'])
+            )
+            
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,17 +1,26 @@
-from django.shortcuts import render, redirect
-
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Phone
 
 def index(request):
     return redirect('catalog')
 
-
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
-    return render(request, template, context)
-
+    
+    sort_param = request.GET.get('sort', 'name')
+    
+    if sort_param == 'name':
+        phones = Phone.objects.all().order_by('name')
+    elif sort_param == 'min_price':
+        phones = Phone.objects.all().order_by('price')
+    elif sort_param == 'max_price':
+        phones = Phone.objects.all().order_by('-price')
+    else:
+        phones = Phone.objects.all()
+    
+    return render(request, template, {'phones': phones})
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
-    return render(request, template, context)
+    phone = get_object_or_404(Phone, slug=slug)
+    return render(request, template, {'phone': phone})
