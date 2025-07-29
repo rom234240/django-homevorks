c596270173ea5630928d7c384b5757d39ae211ec
Sergey Romanov <serhromanov23@gmail.com>
2025-07-29 20:06:20 +0300

    views.py commit

--- 2.2-databases-2/m2m-relations/articles/views.py
+++ 2.2-databases-2/m2m-relations/articles/views.py
@@ -5,10 +5,11 @@ from articles.models import Article
 
 def articles_list(request):
     template = 'articles/news.html'
-    context = {}
+    articles  = Article.objects.order_by('-published_at').prefetch_related('scopes')
+    context = {'object_list': articles}
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = '-published_at'
+    # ordering = '-published_at'
 
     return render(request, template, context)
