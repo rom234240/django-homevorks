51eb90a1f17cdacbd4aaa6d01986fb310667a33f
Sergey Romanov <serhromanov23@gmail.com>
2025-07-31 12:38:13 +0300

    models.py commit

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+
+
+class Sensor(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+
+    def __str__(self):
+        return self.name
+    
+
+class Measurement(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='measurements', verbose_name='Датчик')
+    temperature = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='Температура')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата измерения')
+    image = models.ImageField(upload_to='measurements/', blank=True, null=True, verbose_name='Изображение')
+
+    def __str__(self):
+        return f'{self.sensor} - {self.temperature}°C'
