bd937b7445ee396c269c2fc8bf8542c862624c56
Sergey Romanov <serhromanov23@gmail.com>
2025-08-01 12:09:33 +0300

    filters.py commit

--- 3.3-permissions/api_with_restrictions/advertisements/filters.py
+++ 3.3-permissions/api_with_restrictions/advertisements/filters.py
@@ -6,7 +6,9 @@ from advertisements.models import Advertisement
 class AdvertisementFilter(filters.FilterSet):
     """Фильтры для объявлений."""
 
-    # TODO: задайте требуемые фильтры
+    created_at = filters.DateFromToRangeFilter()
+    status = filters.CharFilter()
 
     class Meta:
         model = Advertisement
+        fields = ['creator', 'status', 'created_at']
